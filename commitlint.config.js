7159d4f9e4e8d0499112dd3c48b0c01fe9bdfa73
Leandro <qazuor@gmail.com>
2025-05-09 22:00:32 +0000

    misc: update commit lint config file

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,31 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'body-max-line-length': [0, 'always'],
-        'footer-max-line-length': [0, 'always']
+        'footer-max-line-length': [0, 'always'],
+        'header-case': [0, 'always', ['sentence-case', 'lower-case']],
+        'body-case': [0, 'always', ['sentence-case', 'lower-case']],
+        'scope-case': [0, 'always', ['lower-case']],
+        'type-case': [0, 'always', ['lower-case']],
+        'subject-case': [0, 'always', ['sentence-case']],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'feat',
+                'fix',
+                'docs',
+                'style',
+                'refactor',
+                'perf',
+                'test',
+                'revert',
+                'build',
+                'ci',
+                'workflow',
+                'types',
+                'del',
+                'misc'
+            ]
+        ]
     }
 };
