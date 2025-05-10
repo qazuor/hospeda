ca8493df344d26211b395e27ddcf2b435e1ad0b5
Leandro <qazuor@gmail.com>
2025-05-10 02:54:18 +0000

    fix: update accord to new types

--- apps/api/src/routes/v1/accommodations.ts
+++ apps/api/src/routes/v1/accommodations.ts
@@ -1,6 +1,6 @@
 import type { Env } from '@/types';
 import { logger } from '@repo/logger';
-import type { Accommodation } from '@repo/types';
+import type { AccommodationType } from '@repo/types';
 import { Hono } from 'hono';
 
 const accommodationRouter = new Hono<Env>();
@@ -10,51 +10,8 @@ accommodationRouter.get('/', async (c) => {
     try {
         const accommodations = [
             {
-                type: 'apartment',
-                id: '1',
-                name: 'Sample Accommodation 1',
-                displayName: 'My Hotel',
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                status: 'active',
-                description: 'This is a sample accommodation description.',
-                rating: 4.5,
-                contactInfo: {
-                    email: 'my_hotel@gmail.com',
-                    phone: '+1234567890'
-                },
-                socialNetworks: {
-                    facebook: 'https://facebook.com/my_hotel',
-                    instagram: 'https://instagram.com/my_hotel',
-                    twitter: 'https://twitter.com/my_hotel'
-                },
-                price: {
-                    basePrice: 100,
-                    currency: 'USD',
-                    additionalFees: {
-                        cleaning: 20,
-                        service: 10,
-                        taxPercentage: 5
-                    },
-                    discounts: {
-                        lastMinute: 10,
-                        weekly: 15,
-                        monthly: 20
-                    }
-                },
-                availability: {
-                    startDate: '2023-10-01',
-                    endDate: '2023-10-31',
-                    availableDates: [{ from: new Date(), to: new Date() }]
-                },
-                ownerId: 'owner_1',
-                location: {
-                    latitude: 40.7128,
-                    longitude: -74.006
-                },
-                amenities: ['WiFi', 'Parking', 'Pool'],
-                images: ['image1.jpg', 'image2.jpg']
-            } as Accommodation
+                // falta poner data
+            } as AccommodationType
         ];
         return c.json({
             success: true,
