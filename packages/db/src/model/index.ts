0b3b7a1a5dafb9f2efd4ad848eed7e0e15c9a8f4
Leandro <qazuor@gmail.com>
2025-05-13 23:34:21 +0000

    feat: export all models

---
+++ packages/db/src/model/index.ts
@@ -0,0 +1,24 @@
+// packages/db/src/model/index.ts
+
+export * from './accommodation.model';
+export * from './accommodationAmenity.model';
+export * from './accommodationFaq.model';
+export * from './accommodationFeature.model';
+export * from './accommodationIaData.model';
+export * from './accommodationReview.model';
+export * from './bookmark.model';
+export * from './destination.model';
+export * from './destinationAttraction.model';
+export * from './destinationReview.model';
+export * from './entityTag.model';
+export * from './event.model';
+export * from './eventLocation.model';
+export * from './eventOrganizer.model';
+export * from './post.model';
+export * from './postSponsor.model';
+export * from './postSponsorship.model';
+export * from './role.model';
+export * from './rolePermission.model';
+export * from './tag.model';
+export * from './user.model';
+export * from './userPermission.model';
