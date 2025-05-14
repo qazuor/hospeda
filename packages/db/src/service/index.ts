b2ae63b48c421334b566e38f132c4ab0d158cc42
Leandro <qazuor@gmail.com>
2025-05-14 21:18:35 +0000

    feat: added destination service

--- packages/db/src/service/index.ts
+++ packages/db/src/service/index.ts
@@ -1,5 +1,6 @@
 export * from './accommodation.service';
 export * from './bookmark.service';
+export * from './destination.service';
 export * from './permission.service';
 export * from './role.service';
 export * from './tag.service';
