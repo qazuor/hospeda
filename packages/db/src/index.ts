5777ab6fa6916bcb1773602ce419df252ae014fe
Leandro <qazuor@gmail.com>
2025-05-21 16:31:36 +0000

    misc: reexport sql value for use in health api endpoint

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -3,3 +3,5 @@ export * from './schema/index.js';
 export * from './service/index.js';
 export * from './types/db-types.js';
 export * from './utils/db-utils.js';
+
+export { sql } from 'drizzle-orm';
