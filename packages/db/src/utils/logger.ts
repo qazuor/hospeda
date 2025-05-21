2b6a5347243a5197a71daed7b70b83388f04cfad
Leandro <qazuor@gmail.com>
2025-05-21 17:39:42 +0000

    feat: added logger util

---
+++ packages/db/src/utils/logger.ts
@@ -0,0 +1,27 @@
+import logger, { type ILogger, LoggerColors, LogLevel } from '@repo/logger';
+
+const dbLogger = logger.registerCategory('Database', 'DB', {
+    color: LoggerColors.BLUE
+});
+
+// Define type for query parameters
+interface QueryParams {
+    table: string;
+    action: string;
+    params: Record<string, unknown>;
+    result: unknown;
+}
+
+// Register the query method
+dbLogger.registerLogMethod<QueryParams>('query', LogLevel.INFO, 'SQL');
+
+// Add this type if you have a logger interface, otherwise define it here
+type DbLogger = ILogger & {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    query: (...args: any[]) => void;
+};
+
+// Cast dbLogger to the correct type if needed
+const typedDbLogger = dbLogger as unknown as DbLogger;
+
+export { typedDbLogger as dbLogger };
