eaaf89f58c2abc70b50a2e84a59b58d259db7c27
Leandro <qazuor@gmail.com>
2025-06-02 13:56:07 +0000

    feat: added permission logger function

--- packages/db/src/utils/logger.ts
+++ packages/db/src/utils/logger.ts
@@ -12,13 +12,25 @@ interface QueryParams {
     result: unknown;
 }
 
+// Define type for query parameters
+interface PermissionValidationParams {
+    permission: string;
+    userId: string;
+    roleId: string;
+    extraData: unknown;
+}
+
 // Register the query method
 dbLogger.registerLogMethod<QueryParams>('query', LogLevel.INFO, 'SQL');
+
+dbLogger.registerLogMethod<PermissionValidationParams>('permission', LogLevel.WARN, 'Permission');
 
 // Add this type if you have a logger interface, otherwise define it here
 type DbLogger = ILogger & {
     // biome-ignore lint/suspicious/noExplicitAny: <explanation>
     query: (...args: any[]) => void;
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    permission: (...args: any[]) => void;
 };
 
 // Cast dbLogger to the correct type if needed
