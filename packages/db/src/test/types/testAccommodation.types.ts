610beba05fc383f3ba5035cbf15cb6c0f215aa80
Leandro <qazuor@gmail.com>
2025-06-03 20:16:29 +0000

    fix: adeded types for tests and improve all comments

---
+++ packages/db/src/test/types/testAccommodation.types.ts
@@ -0,0 +1,5 @@
+import type { CreateInput, UpdateInput } from '../../services/accommodation/accommodation.schemas';
+
+export type TestAccommodationUpdateInput = UpdateInput;
+export type TestAccommodationCreateInput = CreateInput;
+export type PartialAccommodationUpdateInput = Partial<UpdateInput>;
