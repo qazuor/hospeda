70c3453fa894bf8669a7d54f1904542061fe3dc0
Leandro <qazuor@gmail.com>
2025-06-11 00:23:32 +0000

    fix: added index files

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
 export * from './client.ts';
 export * from './dbschemas/index.ts';
+export * from './models/index.ts';
 export * from './utils/index.ts';
 
 export { sql } from 'drizzle-orm';
---
+++ packages/db/src/models/accommodation/index.ts
@@ -0,0 +1,6 @@
+export * from './accommodation.model';
+export * from './accommodation_faq.model';
+export * from './accommodation_ia_data.model';
+export * from './accommodation_review.model';
+export * from './amenity.model';
+export * from './feature.model';
