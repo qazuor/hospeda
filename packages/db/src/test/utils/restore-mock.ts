d13d25717aa2dd22de2fae45d80811537f6a4526
Leandro <qazuor@gmail.com>
2025-06-12 14:05:18 +0000

    misc: moreclaning of db package

--- packages/db/src/test/utils/restore-mock.ts
+++
@@ -1,6 +0,0 @@
-import type { MockInstance } from 'vitest';
-
-export const restoreMock = (fn: unknown) => {
-    const spy = fn as unknown as MockInstance;
-    if (spy.mockRestore) spy.mockRestore();
-};
