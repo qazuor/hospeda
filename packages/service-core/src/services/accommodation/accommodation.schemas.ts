2df9f893e7d775295cfed3b2aeb433d8c715cd6a
Leandro <qazuor@gmail.com>
2025-06-18 17:29:01 +0000

    feat: added search method

--- packages/service-core/src/services/accommodation/accommodation.schemas.ts
+++ packages/service-core/src/services/accommodation/accommodation.schemas.ts
@@ -1,4 +1,5 @@
 import { AccommodationSchema } from '@repo/schemas';
+import { z } from 'zod';
 
 // Schema for creating an accommodation (creation input)
 export const NewAccommodationInputSchema = AccommodationSchema.omit({
@@ -13,3 +14,12 @@ export const NewAccommodationInputSchema = AccommodationSchema.omit({
 
 // Schema for updating an accommodation (update input)
 export const UpdateAccommodationInputSchema = NewAccommodationInputSchema.partial();
+
+export const SearchAccommodationFiltersSchema = z.object({
+    type: z.string().optional(),
+    destinationId: z.string().optional(),
+    amenityIds: z.array(z.string()).optional(),
+    featureIds: z.array(z.string()).optional(),
+    name: z.string().optional(),
+    slug: z.string().optional()
+});
