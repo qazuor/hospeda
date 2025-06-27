a1dd511ea01b2aac77e13b92b4f25fcb84afa1b6
Leandro <qazuor@gmail.com>
2025-06-27 02:09:28 +0000

    fix: update service error code enum

--- packages/types/src/enums/service-error-code.enum.ts
+++ packages/types/src/enums/service-error-code.enum.ts
@@ -10,5 +10,11 @@ export enum ServiceErrorCode {
     /** Unexpected internal error */
     INTERNAL_ERROR = 'INTERNAL_ERROR',
     /** Entity or assignment already exists */
-    ALREADY_EXISTS = 'ALREADY_EXISTS'
+    ALREADY_EXISTS = 'ALREADY_EXISTS',
+    /**
+     * Method is not implemented.
+     * Use this code for public service methods that are stubs or not yet implemented.
+     * Always return this error via the homogeneous pipeline (runWithLoggingAndValidation).
+     */
+    NOT_IMPLEMENTED = 'NOT_IMPLEMENTED'
 }
