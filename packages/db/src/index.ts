93617b1b7d6b8f077ff7f6b8eafca0acd1c15f64
Leandro <qazuor@gmail.com>
2025-07-31 00:56:48 +0000

    misc: reexport export sql for use in api

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,6 @@
+export { sql } from 'drizzle-orm';
 export * from './base/base.model';
+export * from './client';
 export * from './models';
 export * from './schemas';
 export * from './utils';
-export * from './client';
