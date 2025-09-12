633edc2ea12636fb508cec19dea9d69f35e0cc4b
Leandro <qazuor@gmail.com>
2025-09-12 19:44:58 +0000

    feat(schemas): add auth provider enum and user identity schemas
    - Add AuthProviderEnumSchema for OAuth provider validation
    - Add UserIdentityInputSchema for auth provider user data
    - Add UserProfileFromProviderSchema for external profile mapping
    - Export new schemas in index files

--- packages/schemas/src/enums/index.ts
+++ packages/schemas/src/enums/index.ts
@@ -1,5 +1,6 @@
 export * from './accommodation-type.enum.schema.js';
 export * from './amenity-type.enum.schema.js';
+export * from './auth-provider.enum.schema.js';
 export * from './client-type.enum.schema.js';
 export * from './contact-preference.enum.schema.js';
 export * from './currency.enum.schema.js';
@@ -16,4 +17,3 @@ export * from './recurrence.enum.schema.js';
 export * from './role.enum.schema.js';
 export * from './tag-color.enum.schema.js';
 export * from './visibility.enum.schema.js';
-// TODO [3dba642c-6850-4d68-9f56-2f3462635ad2]: add auth-provider enum schema if we need to expose it to clients (z.nativeEnum(AuthProviderEnum))
