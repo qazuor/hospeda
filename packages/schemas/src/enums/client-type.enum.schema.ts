2c5a6adeb91eef2258f26a786dc7b798cbf65262
qazuor <qazuor@gmail.com>
2025-09-22 14:59:31 -0300

    refactor(schemas): Remove deprecated enum schema files

--- packages/schemas/src/enums/client-type.enum.schema.ts
+++
@@ -1,6 +0,0 @@
-import { ClientTypeEnum } from '@repo/types';
-import { z } from 'zod';
-
-export const ClientTypeEnumSchema = z.nativeEnum(ClientTypeEnum, {
-    error: () => ({ message: 'zodError.enums.clientType.invalid' })
-});
--- packages/schemas/src/enums/permission.enum.schema.ts
+++
@@ -1,6 +0,0 @@
-import { PermissionEnum } from '@repo/types';
-import { z } from 'zod';
-
-export const PermissionEnumSchema = z.nativeEnum(PermissionEnum, {
-    error: () => ({ message: 'zodError.enums.permission.invalid' })
-});
