aa77d4ec859b4e5bfb65ffa211b2ddc94fd4d5a1
qazuor <qazuor@gmail.com>
2025-09-24 01:25:04 -0300

    feat(api): clean up route schemas and migrate to @repo/schemas
    - Remove deprecated accommodation and user schema files
    - Update user routes to use @repo/schemas directly
    - Clean up route structure for better maintainability

--- apps/api/src/routes/user/create.ts
+++ apps/api/src/routes/user/create.ts
@@ -4,8 +4,7 @@
  */
 
 import { z } from '@hono/zod-openapi';
-import { LifecycleStatusEnum, RoleEnum, VisibilityEnum } from '@repo/schemas';
-import { UserSchema } from '@repo/schemas';
+import { LifecycleStatusEnum, RoleEnum, UserSchema, VisibilityEnum } from '@repo/schemas';
 import { UserService } from '@repo/service-core';
 import { apiLogger } from '../../utils/logger';
 import { createCRUDRoute } from '../../utils/route-factory';
