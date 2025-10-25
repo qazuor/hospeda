7c47b1abbf5bea2294c96840ab1bcfe704fa3747
qazuor <qazuor@gmail.com>
2025-10-25 09:35:00 -0300

    feat(schemas): add new entity exports to index

--- packages/schemas/src/entities/index.ts
+++ packages/schemas/src/entities/index.ts
@@ -14,6 +14,42 @@ export * from './permission/index.js';
 export * from './post/index.js';
 export * from './postSponsor/index.js';
 export * from './postSponsorship/index.js';
+export * from './product/index.js';
+// TODO [4fcabfc9-aeb6-4701-b367-e50a9ef4cfba]: Enable once migration from old payment model is complete
+// export * from './pricingPlan/index.js';
+// export * from './pricingTier/index.js';
+export * from './client/index.js';
+// TODO [8eda6b67-553c-4fed-bdf7-a9d369a33846]: Enable once migration from old payment model is complete
+// export * from './subscription/index.js';
+export * from './purchase/index.js';
+export * from './subscriptionItem/index.js';
 export * from './tag/index.js';
 export * from './user/index.js';
 export * from './userBookmark/index.js';
+
+// === Billing System Entities ===
+export * from './creditNote/index.js';
+export * from './invoice/index.js';
+export * from './invoiceLine/index.js';
+export * from './paymentMethod/index.js';
+export * from './refund/index.js';
+
+// === Promotions & Discounts System Entities ===
+export * from './discountCode/index.js';
+export * from './promotion/index.js';
+export * from './sponsorship/index.js';
+
+// === Notifications & Marketing System Entities ===
+export * from './notification/index.js';
+
+// === Professional Services System Entities ===
+export * from './professionalService/index.js';
+export * from './serviceOrder/index.js';
+
+// === Benefit Listings System Entities ===
+export * from './benefitListing/index.js';
+
+// === Accommodation Listings System Entities ===
+export * from './accommodationListing/index.js';
+export * from './accommodationListingPlan/index.js';
+export * from './featuredAccommodation/index.js';
